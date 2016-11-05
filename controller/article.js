566173e0ecd52185a42f80b23f803e0f3b734e4a
white-shirt <775321614@qq.com>
2016-11-05 22:45:16 +0800

    添加新动画&&其他视觉效果

---
+++ controller/article.js
@@ -0,0 +1,60 @@
+// 建立mysql连接
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'cheung_ows_com'
+});
+connection.connect();                                                                                                                                       // 响应前端ajax请求
+var pageSize = 10;                                                                                                                                          // 每个page大小是10
+var http = require('http');
+var urllib = require('url');
+http.createServer(
+    function (req, res) {
+        res.charset = 'utf-8';
+        var returnJson;                                                                                                                                     // 将要返回给前端的json串
+        var params = urllib.parse(req.url, true);
+        var total;                                                                                                                                          // 文章总数
+        switch (params.pathname) {
+            case "/getArticle":                                                                                                                             // 如果url是/getArticle就从数据库查寻出结果并返回
+                var returnData = {
+                    totalPage: null,                                                                                                                        // page总数
+                    articles: null,                                                                                                                         // 文章总数
+                    nowPage: params.query.page                                                                                                              // 当前页号,随url传过来
+                };
+                res.writeHead(200, {'Content-Type': 'application/json;charset=utf-8'});
+                var sql = "select count(*) count from article";                                                                                             //从mysql查询总数
+                connection.query(sql, function (err, rows, fields) {                                                                                        // connection。mysql连接
+                    if (err) throw err;
+                    returnJson = rows;
+                    total = rows[0].count;                                                                                                                  // 计算页数
+                    returnData.totalPage = Math.ceil(total / pageSize);                                                                                     //页码总数
+                    var sql = "select * from article limit " + (returnData.nowPage - 1 ) * pageSize + ", " + pageSize
+                    connection.query(sql, function (err, rows, fields) {
+                        if (err) throw err;
+                        returnData.articles = rows;
+                        var str = params.query.callback + '(' + JSON.stringify(returnData) + ')';                                                           // 将returnJson转换成json格式，同时返回给前端
+                        res.end(str);
+                    });
+                });
+                break;
+            case "/delArticle":
+                // todo build sql to del article
+                break;
+            case "/addArticle":
+                // todo add article
+                break;
+            case "/updateArticle":
+                // todo update article
+                break;
+            // 其余情况返回错误
+            default :
+                res.writeHead(200, {'Content-Type': 'text/plain;charset=utf-8'});
+                res.end(JSON.stringify("{'error':'你请求的地址是错的哈哈哈哈'}"));
+                break;
+        }
+
+    }).listen(1337, "127.0.0.1");                                                                                                                           // 监听浏览器的http://127.0.0.1:1337请求
+
+console.log('Server running at http://127.0.0.1:1337/');                                                                                                    // 打印帮助提示
--- js/blog.js
+++ js/blog.js
@@ -1,32 +1,55 @@
 /**
- * Created by Administrator on 2016/10/27.
+ * Created by oneWhitShirt on 2016/10/30.
  */
-var Blog = {
-    animate: {
-        bannerContent: function () {
-            alert(1111111111)
-            alert(1111111111)
-            alert(1111111111)
-            alert(1111111111)
-            alert(1111111111)
-            alert(1111111111)
-            alert(1111111111)
-            111111111
-            alert(1111111111);
-            alert(1111111111)
-            var $bannerContent = $('.bannerContent');
-            $bannerContent.animate({'margin-top': -200 + 'px', 'opacity': 0}, 1000, function () {
-                $bannerContent.remove();
-                console.log(1111);
+function getData(page) {
+    $.ajax({
+        url: 'http://127.0.0.1:1337/getArticle?callback=?',                                                                             // 请求的后端地址
+        data: {page: page},                                                                                                             // 请求的参数
+        type: 'get',                                                                                                                    // 请求的方式，get or post
+        dataType: 'jsonp',                                                                                                              // 预期服务器返回的数据类型
+        success: function (returnData) {                                                                                                // 后端返回成功后前端将会做的处理
+            $('.title,.articleRemark,.abstract,.articleBtn,.articleContent').remove();                                                  //移除旧文章
+            setTimeout(function () {                                                                                                    //刷新页面时，回到最顶端
+                $(document).scrollTop(0);
+            }, 100);
+            var articles = returnData.articles;                                                                                         //后台返回文章
+            for (var i in articles) {
+                var title = $('<div class="title"></div>').text(articles[i].title);
+                var articleRemark = $('<div class="articleRemark"></div>').text(articles[i].articleRemark);
+                var abstract = $('<div class="abstract"></div>').text(articles[i].abstract);
+                var articleBtn = $('<div class="articleBtn"><a>展开/收起</a></div>');
+                var content = $('<div class="articleContent"></div>').text(articles[i].articleContent);
+                $('.wrapInner').append(title, articleRemark, abstract, content, articleBtn);
+            }
+            $(".title").click(function () {                                                                                             //点击标题阅读全文
+                $(this).next().next(".abstract").slideToggle(1000);                                                                     //文章摘要收起
+                $(this).next().next().next(".articleContent").slideToggle(2000);                                                        //内容展开
             });
+            $('.articleBtn').click(function () {
+                $(this).prev('.articleContent').slideToggle(2000);
+                $(this).prev().prev('.abstract').slideToggle(1000);
+            });
+        },
+        error: function () {
+            alert("异常！");
         }
+    })
+}
+getData(1);                                                                                                                             //第一页
+
+//回到顶部
+window.onload = function Top() {
+    var obtn = document.getElementById("arrowTopBtn");                                                                                  //获取回到顶部按钮
+    var timer = null;
+    var ostop = document.documentElement.scrollTop || document.body.scrollTop;
+    obtn.onclick = function () {
+        timer = setInterval(function () {
+            var ostop = document.documentElement.scrollTop || document.body.scrollTop;                                                  // 获取滚动条距离顶部的高度
+            var ispeed = Math.floor(-ostop / 6);                                                                                          //向下取整，滚动条的速度。
+            document.documentElement.scrollTop = document.body.scrollTop = ostop + ispeed;                                              //滚动条距离顶部的高度
+            if (ostop == 0) {
+                clearInterval(timer);
+            }
+        }, 30);
     }
 };
-
-
-// 快捷键
-// reforomate code：ctrl+alt+l
-// 检查语法错误 inspect code  可以自定义快捷键
-// 其他的可以自己定义
-// 还可以定义动态模板，我给你演示下
-// 还可以定义动态模板，我给你演示下
