a4b7a9d1240919a020df83a3613bd589790e67d1
white-shirt <775321614@qq.com>
2017-03-14 23:41:11 +0800

    bg fade add clock

--- js/MobileMain.js
+++ js/MobileMain.js
@@ -6,7 +6,7 @@ var bgFade = function () {
     var $bg = $('.bg');
     var $content = $('.content');
     $bg.animate({opacity: 0}, 3000, function () {
-        $content.animate({bottom: 3 + 'rem'}, 600);
+        $content.animate({bottom: 1.2 + 'rem'}, 600);
     });
 };
 
