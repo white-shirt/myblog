a59a9127fc944a3d2b140daf0e87c0baf71b1513
white-shirt <775321614@qq.com>
2017-04-10 15:02:27 +0800

    remove mobile js

--- js/MobileMain.js
+++ js/MobileMain.js
@@ -1,15 +1,3 @@
 /**
  * Created by Administrator on 2017/3/12.
  */
-
-var bgFade = function () {
-    var $bg = $('.bg');
-    var $content = $('.content');
-    $bg.animate({opacity: 0}, 3000, function () {
-        $content.animate({bottom: 1.2 + 'rem'}, 600);
-    });
-};
-
-$(function () {
-    setTimeout(bgFade, 3000);
-});
--- js/mobileBackground.js
+++ js/mobileBackground.js
@@ -1,6 +1,3 @@
 /**
  * Created by Administrator on 2017/3/12.
  */
-var bgObj = function () {
-    this.animate()
-};
